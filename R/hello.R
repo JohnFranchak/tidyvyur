646e122aa4d0da400dfd5a9e118b47637af5ed3f
John Franchak <franchak@gmail.com>
2022-08-31 22:56:27 -0700

    basic parsing

--- R/hello.R
+++ R/hello.R
@@ -16,6 +16,24 @@ read_unzipped_opf <- function(file_path) {
   require(purrr)
 raw_lines <- read_lines(file_path) %>% discard(~ str_starts(.x, pattern = "#4"))
 column_name_indices <- which(str_detect(raw_lines, pattern = "\\d\\d:\\d\\d", negate = TRUE))
+# data_indices <- which(str_detect(raw_lines, pattern = "\\d\\d:\\d\\d", negate = FALSE))
+
+curr_col_index <- 1
+
+# NEEDS TO ITERATE
+col_header <- raw_lines[column_name_indices[curr_col_index]]
+col_name <- str_split(col_header, pattern = " ")
+arg_names <- str_remove_all(col_name[[1]][[2]],pattern = coll("(MATRIX,true,)-")) |>
+  str_remove_all(pattern = coll("(MATRIX,false,)-")) |>
+  str_remove_all(pattern = coll("|NOMINAL")) |>
+  str_split(pattern = ",") |> unlist()
+
+col <- read_csv(I(raw_lines[{column_name_indices[curr_col_index]+1}:{column_name_indices[curr_col_index+1]-1}]),
+         col_names = c("onset","offset",arg_names)) |>
+  mutate(across(-(onset:offset), ~ str_remove(.x, pattern = "^\\("))) |>
+  mutate(across(-(onset:offset), ~ str_remove(.x, pattern = "\\)$")))
+
 }
+
 
 file_path <- "~/Desktop/4_position_codes"
